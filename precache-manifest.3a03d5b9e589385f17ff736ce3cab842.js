537be6ea1cc38e90a6f5d39e5b119d209acb00d9
lf87 <luke.fryer.email@gmail.com>
2020-06-18 18:00:28 +0100

    first commit

---
+++ precache-manifest.3a03d5b9e589385f17ff736ce3cab842.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b748454abce80b5394a0",
+    "url": "/css/app.78f7f9a0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5b2c3f5427f119f6b66379e53cb01753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b748454abce80b5394a0",
+    "url": "/js/app.859c230c.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "/js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "9a666ec9e9b7cc1a7543408b76cea43d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.3a03d5b9e589385f17ff736ce3cab842.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "itch-bundle-picker"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
